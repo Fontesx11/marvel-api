78dfb5792ac28dcf81b955ca6549491a22fa4867
Diogo Fontes <fontesdgfranca@gmail.com>
2024-02-21 17:34:25 -0300

    feat: Adicionando Api

--- src/pages/HomePage/page.tsx
+++ src/pages/HomePage/page.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios'
+
+const publicKey = '19bb979fb2444ff9653b8dcaedb04cf6'
+
+const hash = '1b572234c3e7464d0d38e8b3caf16033'
 
 export const HomePage = () => {
+
+    useEffect(() =>{
+
+        try {
+
+            const fetch = async () =>{
+                const res =  await axios.get(`http://gateway.marvel.com/v1/public/characters?&ts=1&apikey=${publicKey}&hash=${hash}`)
+                console.log(res.data.data.results)
+            }
+            fetch();
+
+        } catch(err){
+
+        }
+        
+       
+    },[])
 
     return (
         <p>Hello World</p>
